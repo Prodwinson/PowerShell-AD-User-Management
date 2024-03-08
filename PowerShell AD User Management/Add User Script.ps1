adfd187fee175b6ea46ed90caa55421aae662127
Prodwinson <124410666+Prodwinson@users.noreply.github.com>
2024-03-08 13:09:32 -0500

    Add files via upload

---
+++ PowerShell AD User Management/Add User Script.ps1
@@ -0,0 +1,62 @@
+﻿#>
+
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Application]::EnableVisualStyles()
+
+$Form                            = New-Object system.Windows.Forms.Form
+$Form.ClientSize                 = New-Object System.Drawing.Point(400,400)
+$Form.text                       = "Form"
+$Form.TopMost                    = $false
+
+$LableName                       = New-Object system.Windows.Forms.Label
+$LableName.text                  = "Name"
+$LableName.AutoSize              = $true
+$LableName.width                 = 25
+$LableName.height                = 10
+$LableName.location              = New-Object System.Drawing.Point(13,15)
+$LableName.Font                  = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+
+$TextBox1                        = New-Object system.Windows.Forms.TextBox
+$TextBox1.multiline              = $false
+$TextBox1.width                  = 208
+$TextBox1.height                 = 20
+$TextBox1.location               = New-Object System.Drawing.Point(68,17)
+$TextBox1.Font                   = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+
+$ComboBox1                       = New-Object system.Windows.Forms.ComboBox
+$ComboBox1.text                  = ""
+$ComboBox1.width                 = 209
+$ComboBox1.height                = 20
+$ComboBox1.location              = New-Object System.Drawing.Point(68,53)
+$ComboBox1.Font                  = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+$ComboBox1.Items.Add("Users")
+$ComboBox1.Items.Add("Manager")
+$ComboBox1.Items.Add("IT")
+$ComboBox1.Items.Add("Administrator")
+
+$Button1                         = New-Object system.Windows.Forms.Button
+$Button1.text                    = "Add User"
+$Button1.width                   = 60
+$Button1.height                  = 30
+$Button1.location                = New-Object System.Drawing.Point(145,96)
+$Button1.Font                    = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+
+$Form.controls.AddRange(@($LableName,$TextBox1,$ComboBox1,$Button1))
+
+$Button1.Add_Click({ 
+    $userName = $TextBox1.Text
+    $NameInitial = $userName
+    $Name = $userName
+    $samAccountName = $NameInitial
+    $userPrincipleName = $samAccountName + "@winson.local"
+    $path = "OU=" + $ComboBox1.Text + ",OU=Sales, DC=winson, DC=local"
+    $password = ConvertTo-SecureString "Password123!" -AsPlainText -force
+
+    New-ADUser -Name $Name -SamAccountName $samAccountName -UserPrincipalName $userPrincipleName -Path $path -AccountPassword $password -Enabled $true
+})
+
+#region Logic 
+
+#endregion
+
+[void]$Form.ShowDialog()
---
+++ PowerShell AD User Management/User Management Script.ps1
@@ -0,0 +1,21 @@
+﻿# creates a new user with password. Change all the variables to fit your need.
+$userName = "john" 
+$samName = "jdoe"
+$PrincipleName = "jdoe@winson.local"
+$path = "OU=Managers, OU=Sales, DC=winson, DC=local"
+$password = ConvertTo-SecureString "Password123!" -AsPlainText -force
+
+New-ADUser -Name $userName -SamAccountName $samName -UserPrincipalName $PrincipleName -Path $path -AccountPassword $password -enabled $true
+
+# Change $true to $false if you want to disable the user account
+Set-ADUser -Identity $samName -Enabled:$true
+
+# Resetting password for user accounts and changing password at logon
+$NewPassword = "Newpassword123"
+Set-ADAccountPassword -Identity $samName -Reset -NewPassword (ConvertTo-SecureString $NewPassword -AsPlainText -Force)
+Set-ADUser -Identity $samName -ChangePasswordAtLogon $true
+
+# Creating groups and assigning group members
+$groupName = "IT"
+New-ADGroup -Name $groupName -Path $path -GroupScope Global
+Add-ADGroupMember -Identity $groupName -Members $samName
